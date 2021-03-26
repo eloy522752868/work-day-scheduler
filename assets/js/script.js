e664aeb4b8611cc349e9794aa7b27cb32672c6a5
Eloy Gonzalez <eloy522752@gmail.com>
2021-03-26 08:46:01 -0400

    adding changes to table and func.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,33 @@ workskedule = [];
 // Return the hour, according to local time
 var d = new Date();
 var n = d.getHours();
-console.log(n);
 // Call functions 
-for (var i = 6; i < 23; i++) 
-{
- 
-  workskedule.push ({ hour: i , tododesc: "Go to dentist", Button: "<button class = \"saveBtn\" ><i class=\"fas fa-save\"></i></button>"});
+
+var result = [];                      // Results will go here
+var nowHour = new Date().getHours();  // Get current hour of the day
+var timeValue;
+
+
+// Loop from current hour number to 23
+for(var i = 1; i < 25; i++){
+  if (i> 0 && i <= 12) {
+    timeValue=  i + "AM";
+  } else if (i > 12) {
+    timeValue= (i - 12)  + "PM";
+  } else if (i == 0) {
+    timeValue= "PM";
+  }
+ // result.push(i + "00");  // Put loop counter into array with "00" next to it
+  workskedule.push ({ hour: timeValue , tododesc: "Go to dentist", Button: "<button class = \"saveBtn\" ><i class=\"fas fa-save\"></i></button>"});
 }
+
+//console.log(result); // show results
+
+//for (var i = 6; i < 23; i++) 
+//{
+ 
+ // workskedule.push ({ hour: i , tododesc: "Go to dentist", Button: "<button class = \"saveBtn\" ><i class=\"fas fa-save\"></i></button>"});
+//}
 
 
 
@@ -26,28 +46,45 @@ for (var i = 6; i < 23; i++)
 var tableBody = document.getElementById('repo-table');
 //var fetchButton = document.getElementById('fetch-button');
 
+
 function getApi() {
  
   var data = workskedule ;
-
-    
-      console.log(workskedule);
+   
 
       //Loop over the data to generate a table, each table row will have a link to the repo url
       for (var i = 0; i < data.length; i++) {
-        console.log( data[i].hour);
+
         // Creating elements, tablerow, tabledata, and anchor
         var createTableRow = document.createElement('tr');
         var tableDataTime = document.createElement('td');
         var tableDataTodotask = document.createElement('td');
         var tableDatatasksave = document.createElement('td');
         var button = document.createElement('button');
+        var textarea = document.createElement('textarea');
+        textarea.id = "description";
         var btnImage = document.createElement('i');
         button.className = "saveBtn";
         btnImage.className = "fas fa-save";
+        if(i < nowHour)
+        {
+          createTableRow.className = "past";
+        }
+        else if(i === nowHour)
+        {
+          console.log(i);
+          console.log(nowHour);
+          createTableRow.className = "present";          
+        }
+        else if(i > nowHour)
+        {
+          createTableRow.className = "future";
+        }
         // Setting the text of link and the href of the link
         tableDataTime.textContent = data[i].hour;
-        tableDataTodotask.textContent = data[i].tododesc;
+        tableDataTime.className = "hour";
+        textarea.textContent = data[i].tododesc;
+        tableDataTodotask.className = "textarea";
        // tableDatatasksave.textContent = data[i].Button;
       //  link.textContent = data[i].;
       //  link.href = data[i].html_url;
@@ -59,6 +96,7 @@ function getApi() {
         tableBody.appendChild(createTableRow);
 
        // tableData.appendChild(tableDataTodotaske);
+        tableDataTodotask.appendChild(textarea);
         createTableRow.appendChild(tableDataTodotask);
         tableBody.appendChild(createTableRow);
 
