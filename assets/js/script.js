7d65d66a0cce9efc101987d0af093c9d85b3b1ca
Eloy Gonzalez <eloy522752@gmail.com>
2021-03-27 00:27:36 -0400

    cleaning up code add adding comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,40 @@
-var currentTime = moment().format("MMM Do, YYYY, hh:mm:ss");
-//localStorage.removeItem("calanderToDo");
+/**
+* Name: Eloy Gonzalez
+* Date: 03/17/2021
+* Description:
+ This week’s homework requires me create a simple calendar application that allows
+  a user to save events for each hour of the day by modifying starter code. This app will run in the browser and 
+  feature dynamically updated HTML and CSS powered by jQuery. Still learning jquery but try to add as much as I can with impacting
+  my deliverable and meet the requirements. Been working the past week what we have learned on working with API's. I worked in what we have practice. 
+  Very excited what was I working with DOM traversing with this project and getting to work with tables !!
+*/
 
-//addd time
-$("#date-time").text(currentTime);
+/**Decalared Variables
+ * I used the document method querySelector() user moment to get data and working timer intervals to get secound loopthrew.
+ * Below I define the workskedule array and localstorage. Also add a condition validate if array is empty get local storage 
+ * and if it not yet been created generate new data for array and then update local storage.
+ */
+//Used moments and requested
+var currentTime = moment().format("MMM Do, YYYY, hh:mm:ss");
 var timeInterval = null;
 var timeLeft = 60;
-countdown();
-workskedule = [];
+var result = []; // Results will go here
+var nowHour = new Date().getHours(); // Get current hour of the day
+var timeValue;
+var tableBody = document.getElementById("repo-table");
 var storedDayCalander = JSON.parse(localStorage.getItem("calanderToDo"));
+
+
+workskedule = [];
+
+//set moment date time on header
+$("#date-time").text(currentTime);
+//run timer function to loop threw run seconds from moments
+
+
+
+
+//if array is empty after checking for local storage if empty create data for array for day time.
 if (workskedule == null || workskedule.length === 0) {
   var retrievedData = localStorage.getItem("calanderToDo");
   if (retrievedData == null) {
@@ -18,7 +45,6 @@ if (workskedule == null || workskedule.length === 0) {
         hours = hours % 12;
         hours = hours ? hours : 12; // the hour '0' should be '12'
         timeValue = hours + " " + ampm;
-
         workskedule.push({
           hoursampm: timeValue,
           tododesc: "",
@@ -33,20 +59,11 @@ if (workskedule == null || workskedule.length === 0) {
 } 
 else 
 {
-  //alert("Will loop here if data in array")
+  //added is if needed in the future
 }
-// Return the hour, according to local time
-var d = new Date();
-var n = d.getHours();
-// Call functions
 
-var result = []; // Results will go here
-var nowHour = new Date().getHours(); // Get current hour of the day
-var timeValue;
-
-
-var tableBody = document.getElementById("repo-table");
-
+//Calander adding row with data in array , adding class for past 
+//present,future color , adding task , save and storage change in array and local storage.
 function dayCalander() {
   var data = workskedule;
 
@@ -64,16 +81,15 @@ function dayCalander() {
     var btnImage = document.createElement("i");
     button.className = "saveBtn";
     btnImage.className = "fas fa-save";
+    //add classes for css change color
     if (i < nowHour) {
       createTableRow.className = "past";
     } else if (i === nowHour) {
-      console.log(i);
-      console.log(nowHour);
       createTableRow.className = "present";
     } else if (i > nowHour) {
       createTableRow.className = "future";
     }
-    // Setting the text of link and the href of the link
+    //create row for table and td for hour, todo details and button
     tableDataTime.textContent = data[i].hoursampm;
     tableDataTime.className = "hour";
     textarea.textContent = data[i].tododesc;
@@ -81,7 +97,6 @@ function dayCalander() {
     createTableRow.appendChild(tableDataTime);
     tableBody.appendChild(createTableRow);
 
-    // tableData.appendChild(tableDataTodotaske);
     tableDataTodotask.appendChild(textarea);
     createTableRow.appendChild(tableDataTodotask);
     tableBody.appendChild(createTableRow);
@@ -90,37 +105,24 @@ function dayCalander() {
     button.appendChild(btnImage);
     createTableRow.appendChild(tableDatatasksave);
     tableBody.appendChild(createTableRow);
+    //click event for button getting the data for tododesc was pretty tricky but figured it out,
     button.addEventListener("click", function (event) {
- // Create each row's selection to easily choose the appropriate square
-// `eq(0)` or `first()` is the `<h1>`
+      // used jquery as requested
       var row = $(this).parent().parent().children().eq(1).children();
       var prow = $(this).parent().parent();
-      console.log(row[0].value);
-      console.log(prow[0].rowIndex);
-      console.log(workskedule[0].tododesc);
+      //left these consol call do to they help a lot
+     // console.log(row[0].value);
+     // console.log(prow[0].rowIndex);
+     // console.log(workskedule[0].tododesc);
       workskedule[prow[0].rowIndex -1].tododesc = row[0].value
       console.log(workskedule[prow[0].rowIndex -1]);
       localStorage.removeItem("calanderToDo");
       localStorage.setItem("calanderToDo", JSON.stringify(workskedule));
 
       });
-    textarea.addEventListener("change", function (event) {
-      var row = $(this).parent().parent();
-      var rowIndex = $(row[0].rowIndex);
-      
-      console.log(row[0]);
-      //alert(document.getElementById("repo-table").rows.item(rowIndex[0]).innerHTML);
-      // workskedule[rowIndex[0]].tododesc= this.value;
-      // localStorage.removeItem("calanderToDo");
-      // localStorage.setItem("calanderToDo", JSON.stringify(workskedule))
-    //this.textContent = this.value;
-    //dayCalander();
-    
-    });
 
   }
 }
-
 
 function countdown() {
   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
@@ -138,4 +140,6 @@ function countdown() {
   }, 1000);
 }
 
+//call functions
+countdown();
 dayCalander();
